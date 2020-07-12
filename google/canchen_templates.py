663086418c6766b67ce6c2ce17ab3c61d327d6cd
CanChen <bboychencan@gmail.com>
2020-07-12 13:20:26 +0900

    Learned a new algorithm to calculate geometric median. Weiszfeld's algorithm

--- google/canchen_templates.py
+++ google/canchen_templates.py
@@ -403,6 +403,60 @@ def miniMaxScore(i, j, wantMax=True):
 
 
 # Computational Geometry
+## Calculate the geometric median
+## https://en.wikipedia.org/wiki/Geometric_median
+
+def getMinDistSum(self, positions: List[List[int]]) -> float:
+    n = len(positions)
+    if n == 1: return 0.0
+    preans = float('inf')
+    ans = 0
+
+    # Use mean point as the initial point
+    x, y = [sum(i) for i in zip(*positions)]
+    
+    def distancesum(x, y):
+        temp = 0
+        for a, b in positions:
+            temp += math.sqrt(abs(x - a) ** 2 + abs(y - b) ** 2)
+        return temp
+    
+    def bottomsum(x, y):
+        res = 0
+        for a, b in positions:
+            temp = math.sqrt(abs(x - a) ** 2 + abs(y - b) ** 2)
+            # Handle 0 exception
+            if temp == 0: continue
+            res += 1 / temp 
+        return res
+    
+    def uppersum(x, y):
+        xx = 0
+        yy = 0
+        for a, b in positions:
+            temp = math.sqrt(abs(x - a) ** 2 + abs(y - b) ** 2)
+            # Handle 0 exception
+            if temp == 0: continue
+            xx += a / temp
+            yy += b / temp
+        return (xx, yy)
+    
+    def weis(x, y):
+        xx, yy = uppersum(x, y)
+        bottom = bottomsum(x, y)
+        # Handle 0 exception
+        if bottom == 0:
+            return (x, y)
+        return (xx / bottom, yy / bottom)
+    
+    # Iterate Weiszfeld until the improvement is insignificant
+    while abs(ans - preans) > 1e-7:
+        preans = ans
+        x, y = weis(x, y)
+        ans = distancesum(x, y)
+    
+    return ans
+
 ## Valid Square
 def validSquare(self, p1: List[int], p2: List[int], p3: List[int], p4: List[int]) -> bool:
     # Check the distance of every two points, there should be 
